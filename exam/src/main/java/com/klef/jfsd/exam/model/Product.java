1f40a44b580db506f028209bb4c51b489625d0ae
PavanKarthik <karthikpavan370@gmail.com>
2024-12-07 10:29:36 +0530

    repo created and project added

---
+++ exam/src/main/java/com/klef/jfsd/exam/model/Product.java
@@ -0,0 +1,15 @@
+package com.klef.jfsd.exam.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+  
+    private String title;
+    private Double price;
+  
+    private String category;
+    
+
+   
+}
---
+++ exam/src/main/java/com/klef/jfsd/exam/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.klef.jfsd.exam.service;
+
+import com.klef.jfsd.exam.model.Product;
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+}
